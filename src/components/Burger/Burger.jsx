9b88d6741d2e5b1231a4f6178893e255ab26a425
ARSALAN UMAR <64530489+Arxlan40@users.noreply.github.com>
2021-04-13 17:51:16 +0500

    Added Burger :hamburger:

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import Classes from "./Burger.module.css";
+import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
 
-const Burger =(props)=>{
+const Burger = (props) => {
+  return (
+    <div className={Classes.Burger}>
+      <BurgerIngredients type="bread-top" />
+      <BurgerIngredients type="cheese" />
+      <BurgerIngredients type="meat" />
+      <BurgerIngredients type="bread-bottom" />
 
-
-}
+    </div>
+  );
+};
 
 export default Burger;
